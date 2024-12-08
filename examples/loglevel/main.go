e1dd8656f5be2d2425d9da21ff37e14ea83f3bd8
Jeffery Fernandez <jefferyfernandez@gmail.com>
2024-12-09 09:25:12 +1100

    chore: move cmd to examples folder

---
+++ examples/loglevel/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/jeffery/logging/pkg/config"
+	"github.com/jeffery/logging/pkg/logger"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+func main() {
+	envVarFields := map[string]string{
+		"env":     "ENV",
+		"service": "SERVICE",
+		"version": "VERSION",
+	}
+
+	log := config.InitializeLogger(os.Getenv("LOG_LEVEL"), envVarFields)
+
+	// Add service-specific correlation ID if needed
+	log = logger.WithFields(log, logrus.Fields{
+		"correlationId": "123-456-789",
+	})
+
+	log.Trace("This is a trace message")
+	log.Debug("This is a debug message")
+	log.Info("This is an info message")
+	log.Warn("This is a warning message")
+	log.Error("This is an error message")
+}
